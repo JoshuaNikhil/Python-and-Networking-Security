121f8de8a51a970ed0ea92a667084964eac5985b
JoshuaNikhil <58142153+JoshuaNikhil@users.noreply.github.com>
2021-07-05 18:43:17 +0530

    Update hashingwithmd5.py

--- hashingwithmd5.py
+++ hashingwithmd5.py
@@ -1,7 +1,7 @@
 #Import hash library
 import hashlib
 #Intialise String
-string = 'Thankyou'
+string = 'Thankyouharsh'
 #encode string
 #then sending to md5
 encrypted = hashlib.md5(string.encode())
